383a7ba53f4ec1a4f3bcc0199fa9d20a1d830b36
Ethan Gramowski <gramo012@umn.edu>
2025-10-07 14:46:33 -0500

    :)

---
+++ lua/plugins/neotree-persistent.lua
@@ -0,0 +1,26 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  keys = {
+    {
+      "<leader>e",
+      function()
+        require("neo-tree.command").execute({
+          toggle = true,
+          position = "left",
+          source = "filesystem",
+        })
+      end,
+      desc = "Toggle Neo-tree (persistent)",
+    },
+  },
+  opts = {
+    filesystem = {
+      follow_current_file = { enabled = true },
+      window = {
+        mappings = {
+          ["t"] = "open_tabnew", -- open file in new tab
+        },
+      },
+    },
+  },
+}
