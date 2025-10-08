07c36d66b3d1c69cfc8dc1551e5d4b89526a316c
Ethan Gramowski <gramo012@umn.edu>
2025-10-08 10:44:29 -0500

    treesitter and prettier

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "lua",
+        "javascript",
+        "typescript",
+        "html",
+        "css",
+        "python",
+      },
+    },
+  },
+}
